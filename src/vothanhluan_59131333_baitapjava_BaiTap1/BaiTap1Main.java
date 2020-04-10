37875e3cea8605cbc87d15be4cbeb73d8783bdbf
Thanh Luan Vo <vtluan.nt.1999@gmail.com>
2020-04-10 12:56:30 +0700

    BaiTap1

--- src/vothanhluan_59131333_baitapjava_BaiTap1/BaiTap1Main.java
+++ src/vothanhluan_59131333_baitapjava_BaiTap1/BaiTap1Main.java
@@ -14,6 +14,9 @@ public class BaiTap1Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        NhanVien nhanvien1 = new NhanVien("Vo Thanh Luan", 18, "Nha Trang", 1000, 300);
+        NhanVien nhanvien2 = new NhanVien("Lam Mai Huynh", 19, "Nha Trang", 1200, 250);
+        System.out.println(nhanvien1.getThongTin());
+        System.out.println(nhanvien2.getThongTin());
     }
 }
