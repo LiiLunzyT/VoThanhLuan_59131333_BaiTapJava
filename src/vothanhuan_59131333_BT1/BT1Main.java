8e15e718e14593ba535700793b0ff2da02fe58cb
Thanh Luan Vo <vtluan.nt.1999@gmail.com>
2020-04-11 15:34:25 +0700

    BT1

--- src/vothanhuan_59131333_BT1/BT1Main.java
+++ src/vothanhuan_59131333_BT1/BT1Main.java
@@ -15,7 +15,11 @@ public class BT1Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LopHoc lh = new LopHoc();
+        lh.themHocSinh(new HocSinh("Vo Thanh Luan", 21, "Nha Trang", "123", "CNTT1", "IT"));
+        lh.themHocSinh(new HocSinh("Lam Mai Huynh", 20, "Nha Trang", "124", "CNTT2", "IT"));
+        lh.themGVGD(new GiaoVien("Ngo Bao Chau", 50, "Ha Noi", "987", "Toan", "Toan-Tin"));
+        lh.inDSHS();
+        lh.inDSGV();
     }
-    
 }
