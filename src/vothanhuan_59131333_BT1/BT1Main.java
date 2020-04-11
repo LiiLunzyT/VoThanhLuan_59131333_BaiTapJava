9670a1196d9b7aefb4ed5e37e859a79354e41401
Thanh Luan Vo <vtluan.nt.1999@gmail.com>
2020-04-11 13:30:02 +0700

    BaiTap1

---
+++ src/vothanhuan_59131333_BT1/BT1Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vothanhuan_59131333_BT1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BT1Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
